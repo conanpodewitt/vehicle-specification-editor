6aabeb8539a43869ddc6317b1823626f64e4694c
Allen <allenantony2001@gmail.com>
2025-09-04 18:44:58 +0800

    Create more consistent and descriptive file naming and class naming convention

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from PyQt6.QtWidgets import QApplication
-from ui.vcl_widget import VCLEditor
+from ui.main_widget import VCLEditor
 
 TEMP_DIR = os.path.join(os.path.dirname(__file__), 'temp')
 os.makedirs(TEMP_DIR, exist_ok=True)
