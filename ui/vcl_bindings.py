a4a2cc6dcc35d312374a92fbbc8fec9555126c34
duckTapw <lachlan.lego7@gmail.com>
2025-09-30 12:11:35 +0800

    Changed CACHE_DIR from .vehicle_cache to .vehicle

--- ui/vcl_bindings.py
+++ ui/vcl_bindings.py
@@ -8,7 +8,7 @@ from vehicle_lang import VehicleError
 from typing import Sequence, Optional, Callable
 
 
-CACHE_DIR = os.path.join(os.path.expanduser("~"), ".vehicle_cache")
+CACHE_DIR = os.path.join(os.path.expanduser("~"), ".vehicle")
 
 class Runner:
 	def __init__(self, command: str,  script: str = "_run_vcl.py", *args: str, **kwargs: str):
