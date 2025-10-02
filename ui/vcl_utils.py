5b78b4f071eb2297155db078f60f586f3ee4e91a
developing-ar <97691656+developing-ar@users.noreply.github.com>
2025-10-03 05:37:03 +0800

    Utility functions to filter the list json output

---
+++ ui/vcl_utils.py
@@ -0,0 +1,83 @@
+import sys
+import json
+from vehicle_lang.error import VehicleError
+from vehicle_lang.list import list as vcl_list
+from typing import Union
+from pathlib import Path
+
+def list_resources(specification: Union[str, Path]) -> str:
+    """
+    List all networks, datasets, and non-inferable parameters in the specification.
+    :param specification: The path to the Vehicle specification file to list resources for.
+    :return: list of entities as JSON.
+    """
+    try:
+        result = json.loads(vcl_list(specification))
+        filtered_items = []
+        for item in result:
+            item_tag = item["tag"]            
+            if item_tag == "Network" or item_tag == "Dataset" or (item_tag == "Parameter" and item["contents"]["inferable"] == False):
+                filtered_items.append(item)
+        return json.dumps(filtered_items, indent=2)
+    except VehicleError as e:
+        raise VehicleError(f"Error listing resources: {e}")
+
+def list_properties(specification: Union[str, Path]) -> str:
+    """
+    List all properties in specification.
+    :param specification: The path to the Vehicle specification file to list resources for.
+    :return: list of entities as JSON.
+    """
+    try:
+        result = json.loads(vcl_list(specification))
+        filtered_items = []
+        for item in result:
+            item_tag = item["tag"]            
+            if item_tag == "Property":
+                filtered_items.append(item)
+        return json.dumps(filtered_items, indent=2)
+    except VehicleError as e:
+        raise VehicleError(f"Error listing resources: {e}")
+
+def get_resources_info(specification: Union[str, Path]) -> list:
+    """
+    Get resources info from the specification.
+    :param specification: The path to the Vehicle specification file to get resources info for.
+    :return: resources info as a list of dictionaries.
+    """
+    try:
+        resources_json = json.loads(list_resources(specification))
+        resources_info = []
+        for item in resources_json:
+            resources_info.append({"tag": item["tag"], "name": item["contents"]["sharedData"]["name"], "typeText": item["contents"]["sharedData"]["typeText"]})
+        return resources_info
+    except VehicleError as e:
+        raise VehicleError(f"Error getting resources info: {e}")
+
+def get_properties_info(specification: Union[str, Path]) -> list:
+    """
+    Get properties info from the specification.
+    :param specification: The path to the Vehicle specification file to get properties info for.
+    :return: properties info as a list of dictionaries.
+    """
+    try:
+        properties_json = json.loads(list_properties(specification))
+        properties_info = []
+        for item in properties_json:
+            quantified_var_names = [var["sharedData"]["name"] for var in item["contents"]["quantifiedVariables"]]
+            properties_info.append({"name": item["contents"]["sharedData"]["name"], "quantifiedVariablesInfo": quantified_var_names})
+        return properties_info
+    except VehicleError as e:
+        raise VehicleError(f"Error getting properties info: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        file_path = Path(__file__).parent.parent / "temp" / "temp.vcl"
+    results = get_resources_info(file_path)
+    print("--------------Resources---------------")
+    print(json.dumps(results, indent=2))
+    print("--------------Properties---------------")
+    results = get_properties_info(file_path)
+    print(json.dumps(results, indent=2))
