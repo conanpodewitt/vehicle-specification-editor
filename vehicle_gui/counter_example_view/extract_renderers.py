fc9958eef0d52af9d3caeab96b10f3579b3c5f9d
Allen <allenantony2001@gmail.com>
2025-10-03 20:03:06 +0800

    Fix renderer initialisation

--- vehicle_gui/counter_example_view/extract_renderers.py
+++ vehicle_gui/counter_example_view/extract_renderers.py
@@ -2,27 +2,18 @@ import importlib.util
 import inspect
 from typing import Type
 from pathlib import Path
-from ui.counter_example_view.base_renderer import BaseRenderer
+from vehicle_gui.counter_example_view.base_renderer import BaseRenderer
 
 
-def load_renderer_classes(path: str) -> list[Type[BaseRenderer]]:
+def load_renderer_classes(path: str) -> list[BaseRenderer]:
     path = Path(path)
     spec = importlib.util.spec_from_file_location(path.stem, path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
-
     renderers = []
+
     for _, obj in inspect.getmembers(module, inspect.isclass):
         if issubclass(obj, BaseRenderer) and obj is not BaseRenderer:
             renderers.append(obj())
+
     return renderers
-
-
-def initialise_custom_renderers(path: str) -> list[BaseRenderer]:
-    renderer_classes = load_renderer_classes(path)
-    renderer_objects = []
-    
-    for Renderer in renderer_classes:
-        renderer_objects.append(Renderer())
-        
-    return renderer_objects
